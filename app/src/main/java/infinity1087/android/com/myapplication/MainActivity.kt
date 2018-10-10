6eade2c2e8525091fa409634c08977a962c56eea
Natansh Pandya <natansh25@gmail.com>
2018-10-09 22:01:14 -0400

    Initial commit

---
+++ app/src/main/java/infinity1087/android/com/myapplication/MainActivity.kt
@@ -0,0 +1,12 @@
+package infinity1087.android.com.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
