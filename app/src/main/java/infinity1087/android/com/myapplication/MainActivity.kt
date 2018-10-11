093ddd4fdaec96a69c9608d63a8e89e1a4c073b1
Natansh Pandya <natansh25@gmail.com>
2018-10-11 14:47:25 -0400

    implemented camera library

--- app/src/main/java/infinity1087/android/com/myapplication/MainActivity.kt
+++ app/src/main/java/infinity1087/android/com/myapplication/MainActivity.kt
@@ -2,11 +2,37 @@ package infinity1087.android.com.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        camera.setLifecycleOwner(this);
+
+
+        capture.setOnClickListener {
+            camera.capturePicture()
+        }
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        camera.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        camera.stop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        camera.destroy()
+    }
+
+
 }
